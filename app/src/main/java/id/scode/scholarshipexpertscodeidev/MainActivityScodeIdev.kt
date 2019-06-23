668506f3374ecd9ffc64e83a3525095f4d5990fd
yogithesymbian <yogirenbox33@gmail.com>
2019-06-23 12:47:37 -0400

    migrate-androidx-95-persen-succed

--- app/src/main/java/id/scode/scholarshipexpertscodeidev/MainActivityScodeIdev.kt
+++ app/src/main/java/id/scode/scholarshipexpertscodeidev/MainActivityScodeIdev.kt
@@ -1,12 +1,11 @@
 package id.scode.scholarshipexpertscodeidev
 
 import android.content.Intent
-
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import id.scode.exercise01Newbie.ExerciseLatihanSatu
-import id.scode.exercise02MyIntent.IntentScodExercise
+import id.scode.exercise01Newbie.ExerciseLatihanSatuK
+import id.scode.exercise02MyIntent.IntentScodExerciseK
 import id.scode.exercise03ViewAndViewgroup.ViewGroupActivity
 import id.scode.exercise04ReyclerView.ListView
 import id.scode.exercise05Fragment.MyFlexibleFragmentScode
@@ -17,41 +16,85 @@ import kotlinx.android.synthetic.main.activity_main_scode_idev.*
 class MainActivityScodeIdev : AppCompatActivity()
 {
 
+    private val tag = "MainActivityScodeIdev"
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_scode_idev)
 
+        /**
+         * Belajar Android Pemula
+         * ================================================================================
+         */
+
         btnActOpertorScode.setOnClickListener{
             Log.d("MainActivity","Button Operator Clicked")
-            val exerciseOne = Intent(this, ExerciseLatihanSatu::class.java)
+            val exerciseOne = Intent(this@MainActivityScodeIdev, ExerciseLatihanSatuK::class.java)
             startActivity(exerciseOne)
         }
 
         btnMyIntentScode.setOnClickListener{
             Log.d("MainActivity","Button My Intent Clicked")
-            val exerciseTwo = Intent(this, IntentScodExercise::class.java)
+            val exerciseTwo = Intent(this@MainActivityScodeIdev , IntentScodExerciseK::class.java)
             startActivity(exerciseTwo)
         }
 
         btnViewAndViewgroups.setOnClickListener{
             Log.d("MainActivity","Button View and ViewGroups Clicked")
-            val exerciseThree = Intent(this, ViewGroupActivity::class.java)
+            val exerciseThree = Intent(this@MainActivityScodeIdev, ViewGroupActivity::class.java)
             startActivity(exerciseThree)
         }
         btnRecycleView.setOnClickListener{
             Log.d("MainActivity","Button Recycle View Clicked")
-            val rv = Intent(this, ListView::class.java)
+            val rv = Intent(this@MainActivityScodeIdev, ListView::class.java)
             startActivity(rv)
         }
-        btnFragment.setOnClickListener{
-            val fm = Intent(this, MyFlexibleFragmentScode::class.java)
-            startActivity(fm)
-        }
+
 
         btnSubmission.setOnClickListener{
             startActivity(Intent(this@MainActivityScodeIdev, SubmissionHome::class.java))
         }
 
+        /**
+         * Belajar Menjadi Android Developer Expert
+         * ========================================================================
+         */
+
+        btnFragment.setOnClickListener{
+            val fm = Intent(this@MainActivityScodeIdev, MyFlexibleFragmentScode::class.java)
+            startActivity(fm)
+            finish()
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(tag,"onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(tag,"onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(tag,"onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(tag,"onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(tag,"onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(tag,"onDestroy")
     }
 }
